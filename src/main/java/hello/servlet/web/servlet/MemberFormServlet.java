97a48974f8a78fe82c07329432c89a69af49036c
baeksu <kyungwoo5826@naver.com>
2021-11-22 00:49:47 +0900

    study servlet mvc

---
+++ src/main/java/hello/servlet/web/servlet/MemberFormServlet.java
@@ -0,0 +1,42 @@
+package hello.servlet.web.servlet;
+
+import hello.servlet.domain.member.MemberRepository;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+@WebServlet(name = "memberFormServlet", urlPatterns = "/servlet/members/new-form")
+public class MemberFormServlet extends HttpServlet {
+
+    MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        response.setCharacterEncoding("utf-8");
+
+        PrintWriter w = response.getWriter();
+        //위에 url로 요청이 드러오면 아래 html 양식을 뿌려준다.
+        //그리고 웹에서 입력을 받으면 /servlet/members/save 으로 화면을 넘겨준다.-> memberSaveServlet 클래스로 이동하면 출력되는 화면을 볼 수 있다.
+        w.write(
+                "<html>\n" +
+                "<head>\n" +
+                " <meta charset=\"UTF-8\">\n" +
+                " <title>Title</title>\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "<form action=\"/servlet/members/save\" method=\"post\">\n" +
+                " username: <input type=\"text\" name=\"username\" />\n" +
+                " age: <input type=\"text\" name=\"age\" />\n" +
+                " <button type=\"submit\">전송</button>\n" +
+                "</form>\n" +
+                "</body>\n" +
+                "</html>\n");
+    }
+}
