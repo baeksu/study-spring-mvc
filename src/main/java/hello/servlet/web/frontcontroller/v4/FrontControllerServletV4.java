42a7e154c88db40eef4562f0063a287c3a4352ae
baeksu <kyungwoo5826@naver.com>
2021-11-26 21:26:52 +0900

    today study

---
+++ src/main/java/hello/servlet/web/frontcontroller/v4/FrontControllerServletV4.java
@@ -0,0 +1,71 @@
+package hello.servlet.web.frontcontroller.v4;
+
+import hello.servlet.web.frontcontroller.ModelView;
+import hello.servlet.web.frontcontroller.MyView;
+import hello.servlet.web.frontcontroller.v3.ControllerV3;
+import hello.servlet.web.frontcontroller.v3.controller.MemberFormControllerV3;
+import hello.servlet.web.frontcontroller.v3.controller.MemberListControllerV3;
+import hello.servlet.web.frontcontroller.v3.controller.MemberSaveControllerV3;
+import hello.servlet.web.frontcontroller.v4.controller.MemberFormControllerV4;
+import hello.servlet.web.frontcontroller.v4.controller.MemberListControllerV4;
+import hello.servlet.web.frontcontroller.v4.controller.MemberSaveControllerV4;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+//urlPatterns 에 * 표시를 하면 v1 하위의 어떤게 오더라도 이 서블릿이 호출된다는 거다.
+@WebServlet(name = "frontControllerServletV4", urlPatterns = "/front-controller/v4/*")
+public class FrontControllerServletV4 extends HttpServlet {
+
+    private Map<String, ControllerV4> controllerMap = new HashMap<>();
+
+    //생성이 될때 미리 어떤걸 호출할지 저장해 두자
+    public FrontControllerServletV4() {
+        controllerMap.put("/front-controller/v4/members/new-form", new MemberFormControllerV4());
+        controllerMap.put("/front-controller/v4/members/save", new MemberSaveControllerV4());
+        controllerMap.put("/front-controller/v4/members", new MemberListControllerV4());
+    }
+
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("++++++FrontControllerServletV3.service++++++");
+
+        String requestURI = request.getRequestURI();
+
+        ControllerV4 controller = controllerMap.get(requestURI);
+        if (controller == null) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        Map<String, String> paramMap = createParamMap(request);
+        Map<String, Object> model = new HashMap<>();//front controller에서 모델을 직접 만들어 놓는다
+
+        String viewName = controller.process(paramMap,model);
+
+        MyView view = viewResolver(viewName);
+        view.render(model, request, response);
+
+    }
+
+    private MyView viewResolver(String viewName) {
+        System.out.println("/WEB-INF/views/" + viewName + ".jsp");
+        return new MyView("/WEB-INF/views/" + viewName + ".jsp");
+    }
+
+    private Map<String,String> createParamMap(HttpServletRequest request) {
+        //paramMap을 넘겨줘야 한다. v3에서는
+        Map<String, String> paramMap = new HashMap<>();
+        request.getParameterNames().asIterator()
+                .forEachRemaining(paramName -> paramMap.put(paramName, request.getParameter(paramName)));//key변수명을 paramName 으로 잡고 request.getparameter() value를 넣는다
+
+        return paramMap;
+    }
+}
